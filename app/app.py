9519dda49fdef2ca6257904f7e6b9ff38f6a7f32
Spyros Grigoriadis <spyros.grigoriadis@openbet.com>
2025-04-05 21:50:21 +0300

    app

---
+++ app/app.py
@@ -0,0 +1,14 @@
+# app.py
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    name = os.environ.get("NAME", "World")
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
